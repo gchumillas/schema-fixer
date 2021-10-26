f073d02297a5a630c3ba796e7e9dd31addbdafce
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-10-26 17:21:54 +0200

    shorthands

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const { concat } = require('./core/utils')
 const { pipe } = require('./core/pipe')
+const pipes = require('./pipes')
 
 const fix = (value, schema, { fieldPath = '' } = {}) => {
   if (Array.isArray(schema)) {
-    return schema.reduce((prevVal, pipe) => pipe(prevVal, { fieldPath, fix }), value)
+    return schema.reduce((prevVal, pipe) => {
+      const fn = typeof pipe == 'string' ? pipes[pipe]() : pipe
+      return fn(prevVal, { fieldPath, fix })
+    }, value)
   }
 
   return Object.entries(schema).reduce((prevVal, [field, fieldSchema]) => ({
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
 const { fix } = require('./index')
-const { text, float, bool, list, trim, lower, upper } = require('./pipes')
+const { text, float, bool, list } = require('./pipes')
 
 describe('Text validation', () => {
   test('basic', () => {
-    expect(fix('hello there!', [text()])).toBe('hello there!')
-    expect(fix(true, [text()])).toBe('true')
-    expect(fix(125.48, [text()])).toBe('125.48')
+    expect(fix('hello there!', ['text'])).toBe('hello there!')
+    expect(fix(true, ['text'])).toBe('true')
+    expect(fix(125.48, ['text'])).toBe('125.48')
   })
 
   test('require option', () => {
@@ -14,7 +14,7 @@ describe('Text validation', () => {
   })
 
   test('default option', () => {
-    expect(fix(undefined, [text()])).toBe('')
+    expect(fix(undefined, ['text'])).toBe('')
     expect(fix(undefined, [text({ default: undefined })])).toBeUndefined()
     expect(fix(undefined, [text({ default: 'John Smith' })])).toBe('John Smith')
   })
@@ -27,9 +27,9 @@ describe('Text validation', () => {
 
 describe('Float validation', () => {
   test('basic', () => {
-    expect(fix(125.48, [float()])).toBe(125.48)
-    expect(fix('125.48', [float()])).toBe(125.48)
-    expect(() => fix('lorem ipsum', [float()])).toThrow('not a number')
+    expect(fix(125.48, ['float'])).toBe(125.48)
+    expect(fix('125.48', ['float'])).toBe(125.48)
+    expect(() => fix('lorem ipsum', ['float'])).toThrow('not a number')
   })
 
   test('require option', () => {
@@ -37,7 +37,7 @@ describe('Float validation', () => {
   })
 
   test('default option', () => {
-    expect(fix(undefined, [float()])).toBe(0)
+    expect(fix(undefined, ['float'])).toBe(0)
     expect(fix(undefined, [float({ default: undefined })])).toBeUndefined()
     expect(fix(undefined, [float({ default: 125.48 })])).toBe(125.48)
   })
@@ -49,13 +49,13 @@ describe('Float validation', () => {
 
 describe('Boolean validation', () => {
   test('basic', () => {
-    expect(fix(true, [bool()])).toBe(true)
-    expect(fix(false, [bool()])).toBe(false)
-    expect(fix(1, [bool()])).toBe(true)
-    expect(fix(0, [bool()])).toBe(false)
-    expect(fix('', [bool()])).toBe(false)
-    expect(fix('lorem ipsum', [bool()])).toBe(true)
-    expect(fix({}, [bool()])).toBe(true)
+    expect(fix(true, ['bool'])).toBe(true)
+    expect(fix(false, ['bool'])).toBe(false)
+    expect(fix(1, ['bool'])).toBe(true)
+    expect(fix(0, ['bool'])).toBe(false)
+    expect(fix('', ['bool'])).toBe(false)
+    expect(fix('lorem ipsum', ['bool'])).toBe(true)
+    expect(fix({}, ['bool'])).toBe(true)
   })
 
   test('require option', () => {
@@ -63,7 +63,7 @@ describe('Boolean validation', () => {
   })
 
   test('default option', () => {
-    expect(fix(undefined, [bool()])).toBe(false)
+    expect(fix(undefined, ['bool'])).toBe(false)
     expect(fix(undefined, [bool({ default: undefined })])).toBeUndefined()
     expect(fix(undefined, [bool({ default: true })])).toBe(true)
   })
@@ -75,40 +75,40 @@ describe('Boolean validation', () => {
 
 describe('Array validation', () => {
   test('basic', () => {
-    expect(fix([true, false], [list({ type: [text()] })])).toEqual(['true', 'false'])
-    expect(fix([0, 1], [list({ type: [bool()] })])).toEqual([false, true])
-    expect(fix([1, '2', 3], [list({ type: [float()] })])).toEqual([1, 2, 3])
+    expect(fix([true, false], [list({ type: ['text'] })])).toEqual(['true', 'false'])
+    expect(fix([0, 1], [list({ type: ['bool'] })])).toEqual([false, true])
+    expect(fix([1, '2', 3], [list({ type: ['float'] })])).toEqual([1, 2, 3])
   })
 
   test('require option', () => {
-    expect(() => fix(undefined, [list({ type: [float()], require: true })])).toThrow('required')
+    expect(() => fix(undefined, [list({ type: ['float'], require: true })])).toThrow('required')
   })
 
   test('default option', () => {
-    expect(fix(undefined, [list({ type: [text()] })])).toEqual([])
-    expect(fix(undefined, [list({ type: [float()], default: undefined })])).toBeUndefined()
-    expect(fix(undefined, [list({ type: [float()], default: [1, 2, 3] })])).toEqual([1, 2, 3])
+    expect(fix(undefined, [list({ type: ['text'] })])).toEqual([])
+    expect(fix(undefined, [list({ type: ['float'], default: undefined })])).toBeUndefined()
+    expect(fix(undefined, [list({ type: ['float'], default: [1, 2, 3] })])).toEqual([1, 2, 3])
   })
 })
 
 describe('Misc pipelines', () => {
   test('trim', () => {
-    expect(fix(' hello there! ', [text(), trim()])).toBe('hello there!')
-    expect(() => fix(125.48, [trim()])).toThrow('not a string')
+    expect(fix(' hello there! ', ['text', 'trim'])).toBe('hello there!')
+    expect(() => fix(125.48, ['trim'])).toThrow('not a string')
   })
 
   test('lower', () => {
-    expect(fix('Hello There!', [text(), lower()])).toBe('hello there!')
-    expect(() => fix(125.48, [lower()])).toThrow('not a string')
+    expect(fix('Hello There!', ['text', 'lower'])).toBe('hello there!')
+    expect(() => fix(125.48, ['lower'])).toThrow('not a string')
   })
 
   test('upper', () => {
-    expect(fix('hello there!', [text(), upper()])).toBe('HELLO THERE!')
-    expect(() => fix(125.48, [upper()])).toThrow('not a string')
+    expect(fix('hello there!', ['text', 'upper'])).toBe('HELLO THERE!')
+    expect(() => fix(125.48, ['upper'])).toThrow('not a string')
   })
 
   test('combined pipelines', () => {
-    expect(fix(' Hello There! ', [text(), trim(), lower()])).toBe('hello there!')
-    expect(fix(' Hello There! ', [text(), trim(), upper()])).toBe('HELLO THERE!')
+    expect(fix(' Hello There! ', ['text', 'trim', 'lower'])).toBe('hello there!')
+    expect(fix(' Hello There! ', ['text', 'trim', 'upper'])).toBe('HELLO THERE!')
   })
 })
