380e9f32fa1e758a37562711f19aaac84162df3e
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-10-26 11:28:25 +0200

    rename parse by fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const { concat } = require('./core/utils')
 const { pipe } = require('./core/pipe')
 
-const parse = (value, schema, { fieldPath = '' } = {}) => {
+const fix = (value, schema, { fieldPath = '' } = {}) => {
   if (Array.isArray(schema)) {
     return schema.reduce((prevVal, pipe) => pipe(prevVal), value)
   }
 
   return Object.entries(schema).reduce((prevVal, [field, fieldSchema]) => ({
     ...prevVal,
-    [field]: parse(value[field], fieldSchema, { fieldPath: concat([fieldPath, field], '.'), parse })
+    [field]: fix(value[field], fieldSchema, { fieldPath: concat([fieldPath, field], '.'), fix })
   }), {})
 }
 
-module.exports = { parse, pipe }
+module.exports = { fix, pipe }
