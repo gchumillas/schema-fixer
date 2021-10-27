f8d70ee9cb6db8236d6fe3ebd431db1f67427700
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-10-27 20:04:30 +0200

    refactor

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,15 +1,6 @@
-const concat = (texts, glue = '') => {
-  return texts.filter(x => !!x).join(glue)
-}
-
-const isNull = value => {
-  return value === null || value === undefined
-}
-
-const isObject = value => {
-  return value !== null && typeof value == 'object' && !Array.isArray(value)
-}
-
+const concat = (texts, glue = '') => texts.filter(x => !!x).join(glue)
 const error = text => new Error(text)
+const isNull = value => value === null || value === undefined
+const isObject = value => value !== null && typeof value == 'object' && !Array.isArray(value)
 
 module.exports = { concat, isNull, isObject, error }
