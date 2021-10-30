0e6403da47771906714c7a712b8a4764fd858d22
Gonzalo Chumillas <gchumillas@users.noreply.github.com>
2021-10-30 16:25:29 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,8 +2,8 @@ export type Result<T> = [value: T] | [value: undefined, error: any]
 export function error<T>(value: T): [undefined, T]
 export function ok<T>(value: T): [T]
 
-type Pipe<T, S> = (value: any, options: T & { require: boolean, default: any, path: string }) => Result<S>
-type PipeFactory<T, S> = (options?: T & { require?: boolean, default?: any }) => Pipe<T, S>
+export type Pipe<T, S> = (value: any, options: T & { require: boolean, default: any, path: string }) => Result<S>
+export type PipeFactory<T, S> = (options?: T & { require?: boolean, default?: any }) => Pipe<T, S>
 export function pipe<T = {}, S = any>(fn: Pipe<T, S>, options?: { default: S }): PipeFactory<T, S>;
 
 interface SchemaRecord extends Record<string, Schema> { }
