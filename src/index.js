9428c7a62b42221574ce79a77fb21bab78fd07fc
Gonzalo Chumillas <gchumillas@users.noreply.github.com>
2023-01-29 12:35:22 +0100

    remove shorthands

--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
 const { concat, isObject, error, ok } = require('./core/utils')
 const { pipe } = require('./core/pipe')
 const pipes = require('./pipes')
-const { string, number, boolean, trim, lower, upper, array, select } = pipes
-
-const shorthands = {
-  'string': string(),
-  'number': number(),
-  'boolean': boolean(),
-  'trim': trim(),
-  'lower': lower(),
-  'upper': upper(),
-  'string[]': array({ type: [string()] }),
-  'number[]': array({ type: [number()] }),
-  'boolean[]': array({ type: [boolean()] })
-}
-
-const parsePipe = (text = '') => {
-  text = text.trim()
-
-  let fn = shorthands[text]
-  if (!fn) {
-    const matches = text.match(/^\[(.*)]$/)
-    if (matches) {
-      const options = matches[1].split(',').map(x => x.trim())
-      fn = select({ options })
-    }
-  }
-
-  return fn
-}
 
 const parse = (value, schema, { path = '' } = {}) => {
   if (['function', 'string'].includes(typeof schema)) {
@@ -38,16 +10,7 @@ const parse = (value, schema, { path = '' } = {}) => {
   if (Array.isArray(schema)) {
     let acc = value
     for (const pipe of schema) {
-      let fn = pipe
-      if (typeof pipe == 'string') {
-        fn = parsePipe(pipe)
-        if (!fn) {
-          const error = `unrecognized ${pipe} pipe`
-          return [value, [{ path, error }]]
-        }
-      }
-
-      const [val, error] = fn(acc, { path, parse })
+      const [val, error] = pipe(acc, { path, parse })
       if (error) {
         return [value, [{ path, error }]]
       }
