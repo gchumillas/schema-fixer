e252ac4c9b48c7b73d81921db39012ed2627acff
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2024-12-06 18:00:58 +0000

    rename parser to fix

--- src/index.d.ts
+++ src/index.d.ts
@@ -14,18 +14,18 @@ export type Value<T> = T extends FixerRecord
 export function fix<T extends Schema>(value: any, schema: T): Value<T>
 
 // create custom parsers
-declare function parser<T, S>(options: Prettify<{ required: false } & S>): (value: any) => T | undefined
-declare function parser<T, S>(options?: Prettify<{ def?: T } & S>): (value: any) => T
+declare function fixer<T, S>(options: Prettify<{ required: false } & S>): (value: any) => T | undefined
+declare function fixer<T, S>(options?: Prettify<{ def?: T } & S>): (value: any) => T
 declare function createFixer<T, S extends Record<string, any>>(
   def: T,
   fn: (value: any, options: S) => T
-): typeof parser<T, S>
+): typeof fixer<T, S>
 
 // utilities
 export function schema<T extends Schema>(schema: T): (value: any) => Value<T>
 export function join<T>(...fixers: Fixer<T>[]): Fixer<T>
 
-// parsers
+// fixers
 export const text: ReturnType<typeof createFixer<string, { coerce?: boolean }>>
 export const float: ReturnType<typeof createFixer<number, { coerce?: boolean }>>
 export const bool: ReturnType<typeof createFixer<boolean, { coerce?: boolean }>>
